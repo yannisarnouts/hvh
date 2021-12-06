3b3790ec8f162071e2ec3c1a6d709eab61fdf0ae
yannisarnouts <yarnouts@gmail.com>
2021-12-06 15:43:25 +0100

    dialog box

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,61 @@
-import {Component, OnInit} from '@angular/core';
-import { initializeApp } from 'firebase/app';
-import {AngularFirestore} from "@angular/fire/compat/firestore";
+import {Component, Inject, OnInit} from '@angular/core';
 import {Observable} from "rxjs";
 import {NomineeService} from "../services/nominee.service";
+import {MAT_DIALOG_DATA, MatDialog, MatDialogRef} from "@angular/material/dialog";
+
+export interface DialogData {
+  firstname: string;
+  lastname: string;
+  company: string,
+  email: string,
+  phone: string,
+  motivation: string
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   title = 'heldenHepdesk';
   nominees: any[] = new Array();
+  firstname: string = "";
+  lastname: string = "";
+  company: string = "";
+  email: string = "";
+  phone: string = "";
+  motivation: string = "";
 
-  constructor(private nomineeService: NomineeService){}
 
-  ngOnInit(){
-    this.nomineeService.getNominees().subscribe(n => {
-      this.nominees.push(n)
-      console.log(this.nominees);
+  constructor(private nomineeService: NomineeService, public dialog: MatDialog) {
+  }
+
+  ngOnInit() {
+  }
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(NominateDialog, {
+      width: '750px',
+      data: {firstname: this.firstname, lastname: this.lastname, company: this.company, email: this.email, phone: this.phone, motivation: this.motivation },
     });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(result);
+    });
+  }
+}
+@Component({
+  selector: 'nominate-dialog',
+  templateUrl: 'nominateDialog.html',
+})
+export class NominateDialog {
+  constructor(
+    public dialogRef: MatDialogRef<NominateDialog>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData,
+  ) {}
+
+  onNoClick(): void {
+    this.dialogRef.close();
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppComponent, NominateDialog} from './app.component';
 import {environment} from "../environments/environment.prod";
 import {AngularFireModule} from "@angular/fire/compat";
 import {AngularFirestoreModule} from "@angular/fire/compat/firestore";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatDialogModule} from "@angular/material/dialog";
+import {MatInputModule} from "@angular/material/input";
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NominateDialog
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebaseConfig),
-    AngularFirestoreModule
+    AngularFirestoreModule,
+    BrowserAnimationsModule,
+    MatDialogModule,
+    MatInputModule,
+    FormsModule
 ],
   providers: [],
   bootstrap: [AppComponent]
