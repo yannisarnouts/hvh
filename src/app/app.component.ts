9d8671c54eb0ec704681b26f93d44c3cecd801cf
yannisarnouts <yarnouts@gmail.com>
2022-01-06 16:41:52 +0100

    fucking footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {Observable} from "rxjs";
+import {NavigationEnd, Router} from "@angular/router";
 
 @Component({
   selector: 'app-root',
@@ -8,7 +9,18 @@ import {Observable} from "rxjs";
 })
 export class AppComponent implements OnInit {
   title = 'heldenHepdesk';
+  showFooter = false;
+
+  constructor(private router: Router){}
 
   ngOnInit() {
+    const url = this.router.events.subscribe(res => {
+      if (res instanceof NavigationEnd) {
+        if (!res.url.includes('aLogin') && !res.url.includes('admin')) {
+          console.log(res);
+          this.showFooter = true;
+        }
+      }
+    });
   }
 }
